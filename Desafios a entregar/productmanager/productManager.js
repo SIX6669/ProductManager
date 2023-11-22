2bb63da153f0510cea982a809acd7be5f5acd9b5
SIX <simon.494.perrez@gmail.com>
2023-11-22 09:20:52 -0300

    commit

--- Desafios a entregar/productmanager/productManager.js
+++ Desafios a entregar/productmanager/productManager.js
@@ -1,83 +1,103 @@
-let idCounter = 0;
-const fs = require('fs');
 const express = require('express');
+const fs = require('fs');
+let idCounter = 0;
+let cartIdCounter = 0;
 
 class Product {
-    constructor(title, description, price, thumbnail, code, stock){
-        this.id = ++ idCounter;
+    constructor(title, description, price, code, stock, category, status = true, thumbnails = []) {
+        this.id = ++idCounter;
         this.title = title;
         this.description = description;
         this.price = price;
-        this.thumbnail = thumbnail;
         this.code = code;
         this.stock = stock;
+        this.category = category;
+        this.status = status;
+        this.thumbnails = thumbnails;
     }
 }
 
-class ProductManager{
-    constructor(path){
+class Cart {
+    constructor() {
+        this.id = ++cartIdCounter;
+        this.products = [];
+    }
+
+    addProduct(pid) {
+        let product = this.products.find(p => p.product === pid);
+        if (product) {
+            product.quantity++;
+        } else {
+            this.products.push({ product: pid, quantity: 1 });
+        }
+    }
+}
+
+class Manager {
+    constructor(path) {
         this.path = path;
-        this.products = []
+        this.items = [];
     }
 
-    addProduct(product){
-        product.id = ++idCounter;
-        this.products.push(product);
-        fs.writeFileSync(this.path, JSON.stringify(this.products));
+    addItem(item) {
+        this.items.push(item);
+        fs.writeFileSync(this.path, JSON.stringify(this.items));
     }
 
-    getProducts(){
+    getItems() {
         return JSON.parse(fs.readFileSync(this.path));
     }
 
-    getProductById(id){
-        let products = this.getProducts();
-        let product = products.find(product => product.id === id);
-        if (product){
-            return product;
-        }else{
-            console.log('Error: producto no encontrado');
+    getItemById(id) {
+        let items = this.getItems();
+        let item = items.find(item => item.id === id);
+        if (item) {
+            return item;
+        } else {
+            console.log('Error: item no encontrado');
             return null;
         }
     }
 
-    updateProduct(id, field, value){
-        let products = this.getProducts();
-        let product = products.find(product => product.id === id);
-        if (product){
-            product[field] = value;
-            fs.writeFileSync(this.path, JSON.stringify(products));
-        }else{
-            console.log('Error: producto no encontrado');
+    updateItem(id, field, value) {
+        let items = this.getItems();
+        let item = items.find(item => item.id === id);
+        if (item) {
+            item[field] = value;
+            fs.writeFileSync(this.path, JSON.stringify(items));
+        } else {
+            console.log('Error: item no encontrado');
         }
     }
 
-    deleteProduct(id){
-        let products = this.getProducts();
-        let productIndex = products.findIndex(product => product.id === id);
-        if (productIndex !== -1){
-            products.splice(productIndex, 1);
-            fs.writeFileSync(this.path, JSON.stringify(products));
-        }else{
-            console.log('Error: producto no encontrado');
+    deleteItem(id) {
+        let items = this.getItems();
+        let itemIndex = items.findIndex(item => item.id === id);
+        if (itemIndex !== -1) {
+            items.splice(itemIndex, 1);
+            fs.writeFileSync(this.path, JSON.stringify(items));
+        } else {
+            console.log('Error: item no encontrado');
         }
     }
 }
 
 const app = express();
-const myProductManager = new ProductManager('./products.json');
+app.use(express.json());
+const productManager = new Manager('./productos.json');
+const cartManager = new Manager('./carrito.json');
 
-app.get('/products', (req, res) => {
-    let products = myProductManager.getProducts();
+app.get('/api/products', (req, res) => {
+    let products = productManager.getItems();
     let limit = req.query.limit;
     if (limit) {
         products = products.slice(0, limit);
     }
-    res.json({products: products});
+    res.json({ products: products });
 });
 
-app.get('/products/:pid', (req, res) => {
-    const product = myProductManager.getProductById(req.params.pid);
+app.get('/api/products/:pid', (req, res) => {
+    const product = productManager.getItemById(Number(req.params.pid));
     if (product) {
         res.json(product);
     } else {
@@ -85,15 +105,53 @@ app.get('/products/:pid', (req, res) => {
     }
 });
 
-const port = 3000;
+app.post('/api/products', (req, res) => {
+    const { title, description, price, code, stock, category, thumbnails } = req.body;
+    const product = new Product(title, description, price, code, stock, category, true, thumbnails);
+    productManager.addItem(product);
+    res.json(product);
+});
+
+app.put('/api/products/:pid', (req, res) => {
+    const { field, value } = req.body;
+    productManager.updateItem(Number(req.params.pid), field, value);
+    res.send('Producto actualizado');
+});
+
+app.delete('/api/products/:pid', (req, res) => {
+    productManager.deleteItem(Number(req.params.pid));
+    res.send('Producto eliminado');
+});
+
+app.post('/api/carts', (req, res) => {
+    const cart = new Cart();
+    cartManager.addItem(cart);
+    res.json(cart);
+});
+
+app.get('/api/carts/:cid', (req, res) => {
+    const cart = cartManager.getItemById(Number(req.params.cid));
+    if (cart) {
+        res.json(cart);
+    } else {
+        res.status(404).send('Carrito no encontrado');
+    }
+});
+
+app.post('/api/carts/:cid/product/:pid', (req, res) => {
+    const cart = cartManager.getItemById(Number(req.params.cid));
+    if (cart) {
+        cart.addProduct(Number(req.params.pid));
+        cartManager.updateItem(cart.id, 'products', cart.products);
+        res.send('Producto agregado al carrito');
+    } else {
+        res.status(404).send('Carrito no encontrado');
+    }
+});
+
+const port = 8080;
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
-myProductManager.addProduct({title: 'Spaghetti', description: 'spaghettis finos de alta calidad', price: 1500, thumbnail: './creole-spaghetti.jpg', code: 0, stock: 23});
-myProductManager.addProduct({title: 'Canelones rellenos', description: 'canelones rellenos con carne o espinaca', price: 2000, thumbnail: './canelones.jpg', code: 1, stock: 21});
-myProductManager.addProduct({title: 'Sorrentinos', description: 'Deliciosos sorrentinos rellenos de carne, ricota o jamos y queso', price: 2100, thumbnail: './sorrentinos-rellenos-de-jamon-y-queso-con-salsa-marinanara', code: 2, stock: 22});
 
-console.log(myProductManager.getProductById(1));
-console.log(myProductManager.getProductById(2));
-console.log(myProductManager.getProductById(3));
